e60dc97dccb5fbddec58a188a602f3ac06e85aef
RealSakii <maruchop7829@gmail.com>
2026-01-03 06:29:11 -0300

    Update KEYMOHUBPC.lua

--- KEYMOHUBPC.lua
+++ KEYMOHUBPC.lua
@@ -67,8 +67,8 @@ Button.MouseButton1Click:Connect(function()
 
 		--------------------------------
 		-- 🔥 ใส่สคริปหลักของคุณตรงนี้
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/RealSakii/BestAndSpriteScript/refs/heads/main/Functions"))()
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/RealSakii/BestAndSpriteScript/refs/heads/main/Status"))()
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/RealSakii/BestAndSpriteScript/refs/heads/main/KEYMOHUBGUI.lua"))()
 		--------------------------------
 
 	else
@@ -90,6 +90,7 @@ if not getgenv().DisableNotification then
 		Callback = function() end
 	})
 end
+
 
 
 
